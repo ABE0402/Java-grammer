cc9e1d414df6753aa8a9bbcaea3b8b3d9d270d34
ABE0402 <cnrrnquddms@gmail.com>
2025-04-10 18:05:31 +0900

    Add files via upload

---
+++ IMS/dto/SearchDTO.java
@@ -0,0 +1,19 @@
+package dto;
+
+public class SearchDTO {
+	private String searchCondition;
+	private String searchValues;
+	
+	public String getSearchCondition() {
+		return searchCondition;
+	}
+	public void setSearchCondition(String searchCondition) {
+		this.searchCondition = searchCondition;
+	}
+	public String getSearchValues() {
+		return searchValues;
+	}
+	public void setSearchValues(String searchValues) {
+		this.searchValues = searchValues;
+	}
+}
