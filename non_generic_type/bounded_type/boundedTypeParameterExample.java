e282718a535b01f3240f3f5fafda78fda4817aaa
ABE0402 <cnrrnquddms@gmail.com>
2025-04-25 10:36:48 +0900

    Add files via upload

---
+++ non_generic_type/bounded_type/boundedTypeParameterExample.java
@@ -0,0 +1,16 @@
+package bounded_tyoe;
+
+public class boundedTypeParameterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result1 = Util.compare(20, 20);
+		System.out.println(result1);
+		
+		int result2 = Util.compare(4.5, 3);
+		System.out.println(result2);
+		
+//		String str = Util.compare("aa", "bb");
+	}
+
+}
